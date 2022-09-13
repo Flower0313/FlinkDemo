85cba14eb48a1295044339aea254c2cf327a753c
holdenxiao <holdenxiao@163.com>
2022-09-14 00:04:33 +0800

    晚上在看韩老魔

--- src/main/java/com/holden/source/newKafka.java
+++ src/main/java/com/holden/source/newKafka.java
@@ -2,13 +2,28 @@ package com.holden.source;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.holden.bean.SensorReading;
+import lombok.Builder;
+import lombok.Data;
+import org.apache.commons.lang.math.NumberUtils;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.RichMapFunction;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
+import org.apache.flink.streaming.api.functions.ProcessFunction;
+import org.apache.flink.util.Collector;
+import org.apache.flink.util.OutputTag;
+import org.apache.hadoop.yarn.webapp.hamlet2.Hamlet;
 
+import java.math.BigDecimal;
 import java.nio.charset.StandardCharsets;
 
 /**
@@ -21,7 +36,6 @@ public class newKafka {
 
 
     public static void main(String[] args) throws Exception {
-
         StreamExecutionEnvironment env =
                 StreamExecutionEnvironment.getExecutionEnvironment();
         KafkaSource<String> source = KafkaSource.<String>builder()
@@ -34,8 +48,81 @@ public class newKafka {
 
         DataStreamSource<String> kafka_source = env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source");
 
-        kafka_source.map(JSON::parse).print();
+        KeyedStream<Stock, String> keyedStream = kafka_source.map(x -> {
+            JSONObject stockJson = JSON.parseObject(x);
+            return Stock.builder()
+                    .code(stockJson.getString("code"))
+                    .name(stockJson.getString("name"))
+                    .date(stockJson.getString("date"))
+                    .time(stockJson.getString("time"))
+                    .buy1(NumberUtils.createBigDecimal(stockJson.getString("buy1")))
+                    .buy1hand(Long.valueOf(stockJson.getString("buy1hand")))
+                    .sale1(NumberUtils.createBigDecimal(stockJson.getString("sale1")))
+                    .sale1hand(Long.valueOf(stockJson.getString("sale1hand")))
+                    .build();
+        }).keyBy(Stock::getCode);
+
+        OutputTag<String> buyAlertTag = new OutputTag<String>("buyAlert") {
+        };
+        keyedStream.print();
+
+        keyedStream.process(new KeyedProcessFunction<String, Stock, Object>() {
+            private ValueState<BigDecimal> buy1State;
+            private ValueState<BigDecimal> sale1State;
+            private ValueState<Long> buy1HandState;
+            private ValueState<Long> sale1HandState;
+            private ValueState<Long> continuityState;
+
+            @Override
+            public void open(Configuration parameters) throws Exception {
+                buy1State = getRuntimeContext().getState(new ValueStateDescriptor<BigDecimal>("buy1_state", BigDecimal.class));
+                sale1State = getRuntimeContext().getState(new ValueStateDescriptor<BigDecimal>("sale1_state", BigDecimal.class));
+                buy1HandState = getRuntimeContext().getState(new ValueStateDescriptor<Long>("buy1HandState", Long.class));
+                sale1HandState = getRuntimeContext().getState(new ValueStateDescriptor<Long>("sale1HandState", Long.class));
+                continuityState = getRuntimeContext().getState(new ValueStateDescriptor<Long>("continuityState", Long.class, 0L));
+            }
+
+            @Override
+            public void close() throws Exception {
+                buy1State.clear();
+                sale1State.clear();
+                buy1HandState.clear();
+                sale1HandState.clear();
+                continuityState.clear();
+            }
+
+            @Override
+            public void processElement(Stock value, KeyedProcessFunction<String, Stock, Object>.Context ctx, Collector<Object> out) throws Exception {
+                if (buy1State.value() != null || sale1State.value() != null) {
+                    if (value.getBuy1().compareTo(buy1State.value()) > 0) {
+                        if (continuityState.value() >= 4) {
+                            ctx.output(buyAlertTag, value.getCode());
+                        }
+                        // 连续性+1
+                        continuityState.update(continuityState.value() + 1);
+                    } else {
+                        continuityState.update(0L);
+                    }
+                    out.collect(value);
+                }
+            }
+        }).print();
+
 
         env.execute();
     }
+}
+
+
+@Data
+@Builder
+class Stock {
+    private String name;
+    private String code;
+    private String date;
+    private String time;
+    private BigDecimal buy1;
+    private Long buy1hand;
+    private BigDecimal sale1;
+    private Long sale1hand;
 }
--- src/test/java/test1/test.java
+++ src/test/java/test1/test.java
@@ -19,7 +19,7 @@ public class test {
     public static void main(String[] args) {
         String str = "{\"buy4\":\"9.640\",\"date\":\"2022-09-13\",\"buy5\":\"9.630\",\"sale2hand\":\"164700\",\"buy2\":\"9.660\",\"code\":\"sh600352\",\"buy3\":\"9.650\",\"buy1\":\"9.670\",\"sale1\":\"9.680\",\"buy4hand\":\"112800\",\"sale2\":\"9.690\",\"sale4hand\":\"250110\",\"buy2hand\":\"100300\",\"sale5\":\"9.720\",\"sale3\":\"9.700\",\"sale4\":\"9.710\",\"sale3hand\":\"70300\",\"buy1hand\":\"96081\",\"sale1hand\":\"110700\",\"name\":\"浙江龙盛\",\"sale5hand\":\"342300\",\"buy3hand\":\"123800\",\"time\":\"15:00:00\",\"buy5hand\":\"115300\"}";
 
-        System.out.println(JSON.parse(str));
+        System.out.println(JSON.parseObject(str));
 
     }
 }
